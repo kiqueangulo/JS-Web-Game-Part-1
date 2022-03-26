0ce30c0c31d8be5773f69e3aad44ceb7a6a97e91
kiqueangulo <kiqueangulo@gmail.com>
2022-03-26 18:55:47 -0400

    clean without background

--- real.js
+++ real.js
@@ -1,54 +1,161 @@
-// Function to create, positionate, and add an image to the page
-function newImage(imgAddress, leftPosition, bottomPosition) {
-    let imgName = document.createElement('img');
-    imgName.src = imgAddress;
-    imgName.style.position = 'fixed';
-    imgName.style.left = leftPosition + 'px';
-    imgName.style.bottom = bottomPosition + 'px';
-    document.body.append(imgName);
-    return imgName; // I have to return it so I can can call it and assign it to another variable
+// Function to create images
+function newImage(url){
+    let image = document.createElement('img')
+    image.src = url
+    document.body.append(image)
+    return image // Return to use it later
 }
 
-// Function to make an image disappear (being picked up)
-function newItem(itemAddress, leftPosition, bottomPosition) {
-    let itemName = newImage(itemAddress, leftPosition, bottomPosition); // I call a function within a function by assigning it to a variable
-
-    itemName.addEventListener('dblclick', function() {
-        itemName.remove();
+// Function to create new items
+function newItem(url){
+    let item = newImage(url)
+    // Event to make the item go to inventory
+    item.addEventListener('click', () => {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url;
+        inventory.append(inventoryItem)
     })
+    return item
 }
 
-// Function to generate the background
+// Function to ubicate/guide elements
+function move(element) {
+    element.style.position = 'fixed'
+
+    // To place items/images
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    // To move the pc
+    function moveWithArrowKeys(left, bottom, callback) {
+        let direction = null; // Trigger variable
+        let x = left;
+        let y = bottom;
+        
+        element.style.left = x + 'px';
+        element.style.bottom = y + 'px';
+        
+        // Updating the pc's position every 1ms
+        setInterval(() => {
+            if (direction === 'west') {
+                x -= 1;
+            };
+            if (direction === 'north') {
+                y += 1;
+            };
+            if (direction === 'east') {
+                x += 1;
+            };
+            if (direction === 'south') {
+                y -= 1;
+            };
+
+            if (x <= 0 || x === window.innerWidth) {
+                element.style.left = '0px';
+            }
+
+            element.style.left = x + 'px';
+            element.style.bottom = y + 'px';
+        }, 1);
+
+        // Assigning intuitive behavior to arrow keys
+        document.addEventListener('keydown', function(e) {
+            if(e.repeat) return;
+
+            if (e.key === 'ArrowLeft') {
+                direction = 'west';
+            };
+            if (e.key === 'ArrowUp') {
+                direction = 'north';
+            };
+            if (e.key === 'ArrowRight') {
+                direction = 'east';
+            };
+            if (e.key === 'ArrowDown') {
+                direction = 'south';
+            };
+
+            callback(direction); // To call the illusion function
+        });
+
+        document.addEventListener('keyup', function(e) {
+            direction = null;
+            callback(direction);
+        });
+    }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys,
+    }
+}
+
+// Function to create the inventory
+function newInventory(){
+    let inventory = document.createElement('div')
+    inventory.style.width = '100%'
+    inventory.style.height = '100px'
+    inventory.style.display = 'flex'
+    inventory.style.flexDirection = 'row'
+    inventory.style.alignItems = 'center'
+    inventory.style.justifyContent = 'space-evenly'
+    inventory.style.border = '2px solid black'
+    inventory.style.backgroundColor = 'brown'
+    document.body.append(inventory)
+    return inventory
+}
+
+const inventory = newInventory() // Making scope inventory general
+move(inventory).to(0, 0)
+
+const character = newImage('assets/green-character/static.gif') // Playable character (pc)
+
+// Function to give the impression of changing directions
+function handleDirectionChange(direction) {
+    if (direction === null) {
+        character.src = 'assets/green-character/static.gif';
+    };
+    if (direction === 'west') {
+        character.src = 'assets/green-character/west.gif';
+    };
+    if (direction === 'north') {
+        character.src = 'assets/green-character/north.gif';
+    };
+    if (direction === 'east') {
+        character.src = 'assets/green-character/east.gif';
+    };
+    if (direction === 'south') {
+        character.src = 'assets/green-character/south.gif';
+    };
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/sheild.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
+
+/* This was supposed to be the back ground, but I have to figure out why it's not working
+
 function background(backgroundAddress, leftPosition, bottomPosition, leftIndentantionFactor, bottomIndentantionFactor) {
-    for(let h = 0; h < bottomIndentantionFactor; h++) { // This will be adding the image vertically
-        for(let w = 0; w < leftIndentantionFactor; w++) { // This will be adding the image horizontally
+    for(let h = 0; h < bottomIndentantionFactor; h++) {
+        for(let w = 0; w < leftIndentantionFactor; w++) {
             newImage(backgroundAddress, leftPosition + w * 100, bottomPosition + h * 100);
-            // Times 100 because that's the width and height of the images I'm using, so the next will be added right next to the previous one
         }
     }
 }
 
-let groundHeight = window.innerHeight * 0.64; // Total height of the grass within the viewport
-let skyHeight = window.innerHeight - groundHeight; // The remmaining space of the viewport
+let groundHeight = window.innerHeight * 0.64;
+let skyHeight = window.innerHeight - groundHeight;
 
-background('assets/grass.png', '0', '0', window.innerWidth / 100, groundHeight / 100); // Divided by 100 to make it a factor of the width and height of the images
+background('assets/grass.png', '0', '0', window.innerWidth / 100, groundHeight / 100);
 
-background('assets/sky.png', '0', groundHeight, window.innerWidth / 100, skyHeight / 100); //I use groundHeight as bottomPosition to begin the sky where the grass ends
-
-newImage('./assets/green-character.gif', '100', '100');
-
-newImage('./assets/pine-tree.png', '450', '200');
-
-newImage('assets/tree.png', '200', '300');
-
-newImage('assets/pillar.png', '350', '100');
-
-newImage('assets/crate.png', '150', '200');
-
-newImage('assets/well.png', '500', '450');
-
-newItem('assets/sword.png', '500', '405');
-
-newItem('assets/sheild.png', '165', '185');
-
-newItem('assets/staff.png', '600', '100');
+background('assets/sky.png', '0', groundHeight, window.innerWidth / 100, skyHeight / 100); */
